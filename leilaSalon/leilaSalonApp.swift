d84d0270663729700477df046b41cf609bf69e79
Izadora Lima <contato.izadoramatias@gmail.com>
2026-06-16 10:21:15 -0300

    Initial Commit

---
+++ leilaSalon/leilaSalonApp.swift
@@ -0,0 +1,32 @@
+//
+//  leilaSalonApp.swift
+//  leilaSalon
+//
+//  Created by Izadora Lima on 16/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct leilaSalonApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ leilaSalonTests/leilaSalonTests.swift
@@ -0,0 +1,17 @@
+//
+//  leilaSalonTests.swift
+//  leilaSalonTests
+//
+//  Created by Izadora Lima on 16/06/26.
+//
+
+import Testing
+@testable import leilaSalon
+
+struct leilaSalonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
